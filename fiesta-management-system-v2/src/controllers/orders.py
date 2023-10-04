5d7ed1ca1a88921993a74cceff60329261bf96a9
= <=>
2023-10-04 11:50:29 +0530

    -

--- fiesta-management-system-v2/src/controllers/orders.py
+++ fiesta-management-system-v2/src/controllers/orders.py
@@ -1,12 +1,12 @@
 from src.models.database import Database
+from src.utils import config
 
 
 class Orders:
 
     def check_order(self, user_id=None, date=None):
         db = Database()
-        query = "select id, user_id, amount, created_at from orders where user_id = %s order by created_at desc;"
-        response = db.get_items(query, (user_id,))
+        response = db.get_items(config.queries["CHECK_ORDER"], (user_id,))
         if date is None:
             return [dict(order_id=item[0], user_id=item[1], amount=item[2], date=str(item[3])[:10]) for item in
                     response]
@@ -18,6 +18,6 @@ class Orders:
     @staticmethod
     def store_order(user_id, amount, created_by):
         db = Database()
-        order_id = db.add_item("insert into orders(user_id, amount, created_by) values(%s,%s,%s)",
+        order_id = db.add_item(config.queries["STORE_ORDER"],
                                (user_id, amount, created_by))
         return order_id
--- fiesta-management-system-v2/src/controllers/user.py
+++ fiesta-management-system-v2/src/controllers/user.py
@@ -4,6 +4,7 @@ from src.controllers.feedback import Feedback
 from src.controllers.menu import Menu
 from src.controllers.orders import Orders
 from src.models.database import Database
+from src.utils import config
 
 
 class User(Menu, Orders, Account, Feedback, Criteria):
@@ -13,12 +14,10 @@ class User(Menu, Orders, Account, Feedback, Criteria):
         self.grp_id = grp_id
         self.role = role
 
-    def get_details(self, user_name = None):
+    def get_details(self, user_name=None):
         db = Database()
-        query = ("select a.id, a.user_name, r.role, u.grp_id from authentication as a inner join user_roles as r on ("
-                 "a.id = r.user_id) inner join users as u on (u.user_id = a.id) where user_name = %s")
-        response = db.get_items(query, (self.user_name,))
-        print(response)
+        response = db.get_items(config.queries["GET_DETAILS"], (self.user_name,))
+        # print(response)
         data = dict(
             user_id=response[0][0],
             user_name=response[0][1],
@@ -31,14 +30,6 @@ class User(Menu, Orders, Account, Feedback, Criteria):
 
     def get_users(self, grp_id):
         db = Database()
-        query = "select user_id, balance from user_balance where grp_id=%s;"
-        response = db.get_items(query, (grp_id,))
+        response = db.get_items(config.queries["GET_USERS"], (grp_id,))
         if response is not None:
             return [dict(user_id=item[0], balance=item[1]) for item in response]
-
-
-
-if __name__ == "__main__":
-    user = User()
-    resp = user.get_users(grp_id=1)
-    print(resp)
